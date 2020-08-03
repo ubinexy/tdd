8f15472e0e53df1755965ccb3630fa5a0a509183
ubinexy <ubinexy@gmail.com>
2020-08-03 20:46:34 +0800

    Add tests

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -13,8 +13,25 @@ public class MommifierTest {
     @Test
     void should_insert_mommy_when_vowel_split_give_string_with_vowels_higher_then_30_percent() {
         Mommifier mommifier = new Mommifier();
+        String origin = "zippa";
+        String expect = "zippa";
+        Assertions.assertEquals(expect, mommifier.vowelSplit(origin));
+    }
+
+    @Test
+    void should_return_origin_when_vowel_split_give_string_with_vowels_higher_then_30_percent_but_not_continue() {
+        Mommifier mommifier = new Mommifier();
         String origin = "hear";
         String expect = "hemommyar";
+        Assertions.assertEquals(expect, mommifier.vowelSplit(origin));
+    }
+
+
+    @Test
+    void should_insert_mommy_when_vowel_split_give_string_with_Capital_vowels_higher_then_30_percent() {
+        Mommifier mommifier = new Mommifier();
+        String origin = "HEAR";
+        String expect = "HEmommyAR";
         Assertions.assertEquals(expect, mommifier.vowelSplit(origin));
     }
 }
